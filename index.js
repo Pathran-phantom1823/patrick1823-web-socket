eefe6dfcff59cb21a96bae79140f755c075a96cd
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-10-02 13:51:18 +0800

    finalizing

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 3000;
 // var mqtt = require('mqtt');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
  });
 
 app.use(express.static('public'));
